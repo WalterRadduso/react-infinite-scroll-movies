80a809e28b3caf7a4c1c698d26720f49a022b8b5
Walter Radduso <w.radduso@gmail.com>
2020-09-28 21:35:06 -0300

    [FTR] - Added dependencies: node-sass, prettier, react-router-dom, axios and classnames. - Added prettierrc and eslintrc files.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React Infinite Scroll Movies Project.
-        </p>
+        <p>React Infinite Scroll Movies Project.</p>
       </header>
     </div>
   );
